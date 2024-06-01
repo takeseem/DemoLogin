ae03750fa640a687a7375ce49a8825953ab7d36a
杨浩 <takeseem@gmail.com>
2024-06-01 12:21:25 +0800

    添加 Login HeaderText

--- app/src/main/java/com/takeseem/app/demo/login/ui/login/LoginScreen.kt
+++ app/src/main/java/com/takeseem/app/demo/login/ui/login/LoginScreen.kt
@@ -1,10 +1,25 @@
 package com.takeseem.app.demo.login.ui.login
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.takeseem.app.demo.login.ui.components.HeaderText
+
+val defaultPadding = 16.dp
 
 @Composable
 fun LoginScreen() {
+	Column(
+		modifier = Modifier.fillMaxSize().padding(defaultPadding),
+		horizontalAlignment = Alignment.CenterHorizontally
+	) {
+		HeaderText(text = "Login", modifier = Modifier.padding(vertical = defaultPadding).align(Alignment.Start))
+	}
 }
 
 @Preview
